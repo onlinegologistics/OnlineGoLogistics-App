269db7b6481c2558a39f14d475aa26c548ffa37d
worknaiintern3 <worknaiintern3@gmail.com>
2026-07-27 15:29:57 +0530

    fix: Add nodemailer timeout to prevent 504 errors

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -6,6 +6,10 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    // Add timeouts so it doesn't hang indefinitely (fixes 504 Gateway Timeout)
+    connectionTimeout: 5000,
+    greetingTimeout: 5000,
+    socketTimeout: 5000,
 });
 
 module.exports = transporter;
